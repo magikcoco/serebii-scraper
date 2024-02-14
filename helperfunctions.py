9237bbfc5a8f09bf5a0fcf6aa5df3aa8975985f7
magikcoco <magikcoco.dev@gmail.com>
2024-02-14 07:43:27 -0500

    fixed some critical errors with logging in the request_page() function

--- helperfunctions.py
+++ helperfunctions.py
@@ -2,8 +2,38 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import logging
+from datetime import datetime
 
-start_timestamp = None
+initial_timestamp = None
+
+def setup_logger():
+    global initial_timestamp
+
+    if initial_timestamp is None:
+        initial_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    logger = logging.getLogger('my_shared_logger')
+    logger.setLevel(logging.DEBUG)
+
+    log_filename = f'shared_log_{initial_timestamp}.log'
+
+    # Create handlers
+    file_handler = logging.FileHandler(log_filename, mode='a')
+    console_handler = logging.StreamHandler()
+
+    # Create formatters and add it to handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add handlers to the logger
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logger()
 
 def wild_hold_item_parse(text):
     return_dict = {}
@@ -30,36 +60,9 @@ def request_page(url):
     """
     #time.sleep(5) # for avoiding a rate limit
     response = requests.get(url)
-    print(f"Webpage response: {response.status_code}")
+    logger.info(f"Webpage response: {response.status_code}")
     if response.status_code == 200:
         return BeautifulSoup(response.text, 'html5lib') #magic parsing library do not touch
     else:
         #TODO: handle other errors
         return None
-
-def setup_logger():
-    global initial_timestamp
-
-    if initial_timestamp is None:
-        initial_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-    logger = logging.getLogger('my_shared_logger')
-    logger.setLevel(logging.DEBUG)
-
-    log_filename = f'shared_log_{initial_timestamp}.log'
-
-    # Create handlers
-    file_handler = logging.FileHandler(log_filename, mode='a')
-    console_handler = logging.StreamHandler()
-
-    # Create formatters and add it to handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
-
-    # Add handlers to the logger
-    if not logger.handlers:
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-
-    return logger
